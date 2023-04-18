09e6b16697d85aadf5070d5da291eeb206422bc6
Julien GOLETTO <juliengoletto@MacBook-Pro-de-Julien.local>
2023-04-18 16:41:14 +0200

    :test_tube: Finished 2.2

--- src/__tests__/exercices/2.test.tsx
+++ src/__tests__/exercices/2.test.tsx
@@ -1,33 +1,24 @@
-import { act } from 'react-dom/test-utils';
 import { describe, test, expect } from 'vitest';
 import { screen, render } from '@testing-library/react';
 import { Counter } from '../../components/counter/Counter';
+import userEvent from '@testing-library/user-event';
 
-const click = async (element: HTMLElement) => {
-  const event = new MouseEvent('click', {
-    bubbles: true,
-    cancelable: true,
-  });
-
-  await act(() => {
-    element.dispatchEvent(event);
-  });
-};
+const user = userEvent.setup();
 
 describe('Counter', () => {
   test('the buttons minus and plus are functionnal when clicked', async () => {
     // 🦁 Utilise `render` de `@testing-library/react` pour rendre le composant `Counter`
-    const { container } = render(<Counter />);
+    render(<Counter />);
     // 🦁 Récupérer le span qui contient le nombre avec screen.getByText
     const numberCounter = screen.getByText("0");
     // // 🦁 Récupère le bouton plus avec `screen.getByRole`
     const [plus, minus] = screen.getAllByRole("button");
     // 🦁 Clique sur le bouton "plus"
-    await click(plus);
+    await user.click(plus);
     // 🦁 Vérifie que le contenue du span est "1"
     expect(numberCounter.textContent).toBe("1");
     // 🦁 Clique sur le bouton "minus"
-    await click(minus);
+    await user.click(minus);
     // 🦁 Vérifie que le contenue du span est "0"
     expect(numberCounter.textContent).toBe("0");
   });
