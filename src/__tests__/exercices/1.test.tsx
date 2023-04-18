f891343f64501255b3dd91773c05a13c2104b656
Julien GOLETTO <juliengoletto@MacBook-Pro-de-Julien.local>
2023-04-18 11:13:00 +0200

    :test_tube: Exercise 1.1 done

--- src/__tests__/exercices/1.test.tsx
+++ src/__tests__/exercices/1.test.tsx
@@ -1,25 +1,44 @@
 // 🦁 Ajoute `expect` à l'import de `vitest`
-import { describe, test } from 'vitest';
+import { describe, test, expect } from 'vitest';
 // 🦁 Dé-commente les imports suivants
-// import ReactDOM from 'react-dom/client';
-// import { Counter } from '../../components/1/Counter';
-// import { wait } from '../../test/wait';
+import ReactDOM from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
+import { Counter } from '../../components/counter/Counter';
+import { wait } from '../../test/wait';
 
 describe('Counter', () => {
   test('the counter is decremented when the minus button is clicked', async () => {
     // 🦁 Créer un élément `div` avec `document.createElement`
+    const div = document.createElement('div');
     // 🦁 Ajouter l'élément `div` au `body` avec `document.body.append`
+    document.body.append(div);
     // ---
     // 🦁 Il faut wrapper la suite dans un `act` comme expliqué dans les instructions
     // 🦁 Créer un `root` avec `ReactDOM.createRoot`
     // 🦁 Rendre le composant `Counter` dans le `root`
+    await act(() => {
+      const root = ReactDOM.createRoot(div);
+      root.render(<Counter />);
+    });
     // --
     // 🦁 Récupère le bouton "minus"
+    const buttons = [...document.querySelectorAll('button')];
+    const minusButton = buttons.find((b) => (b.textContent === '-'));
     // 🦁 Utilise `expect` pour vérifier que le contenue du span est "0"
+    const counterNumber = document.querySelector('span');
+    expect(counterNumber?.textContent).toBe('0');
     // --
     // 🦁 Il faut wrapper l'appel du clique dans un `act`
-    // 🦁 Clique sur le bouton "plus"
-    // 🦁 Attendre 1ms
-    // 🦁 Vérifie que le contenue du span est "1"
+    await act(async () => {
+      // 🦁 Clique sur le bouton "moins"
+      minusButton?.click();
+      // 🦁 Attendre 1ms
+      // setTimeout(() => {
+      //   return;
+      // }, 1000);
+      await wait(1);
+    });
+    // 🦁 Vérifie que le contenue du span est "-1"
+    expect(counterNumber?.textContent).toBe('-1');
   });
 });
